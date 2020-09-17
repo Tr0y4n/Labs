7b7a6f7abe32045f08d6f89c746676b6d3ffcf95
Tr0y4n <55826971+Tr0y4n@users.noreply.github.com>
2020-09-17 15:27:01 +0300

    Исправил все, что нужно исправить

--- VUZ.py
+++ VUZ.py
@@ -3,25 +3,19 @@ import requests
 from bs4 import BeautifulSoup
 import datetime
 
-def first_task():
-    arr = input("Enter the array: ").splice(" ")
+def dictify(arr):
     dic = { arr[i]:list(k for k in range(i, len(arr)) if arr[i] == arr[k]) for i in range(0,len(arr)) if arr.index(arr[i]) >= i}
     print(dic)
 
-def second_task():
-    first_set = input("Введите первое множество: ").split(" ")
-    second_set = input("Введите второе множество: ").split(" ")
+def jaccard(first_set, second_set):
     intersections = 0
     union = len(first_set) + len(second_set)
-    for i in first_set:
-        for j in second_set:
-            if i == j:
-                union -= 1
-                intersections += 1
-    result = intersections / union
-    return result
+    for item in first_set:
+        if item in second_set:
+            intersections += 1
+    print(intersections / (len(first_set) + len(second_set) - intersections))
 
-def fourth_task():
+def Json_to_csv():
     jname = "data.json"
     csvname = "result.csv"
 
@@ -38,7 +32,7 @@ def fourth_task():
                 for year in value:
                     writer.writerow({"item": dict['item'], "country": country, "year": year, "sales": value[year]})
 
-def fith_task():
+def currency():
 
     start = datetime.datetime.strptime("01/03/2020", "%d/%m/%Y")
     end = datetime.datetime.strptime("01/07/2020", "%d/%m/%Y")
@@ -52,7 +46,18 @@ def fith_task():
         for date in date_generated:
             resp = requests.get("http://www.cbr.ru/scripts/XML_daily.asp?date_req=" + date.strftime("%d/%m/%Y"))
             soup = BeautifulSoup(resp.content, "xml")
-            writer.writerow({"Дата": date.strftime("%d/%m/%Y"), "Доллар США": soup.find("CharCode", text = "USD").find_next_sibling("Value").string,
-            "Евро": soup.find("CharCode", text = "EUR").find_next_sibling("Value").string,
-            "Индийская рупия": soup.find("CharCode", text = "INR").find_next_sibling("Value").string,
-            "Украинская гривна": soup.find("CharCode", text = "UAH").find_next_sibling("Value").string})
+            writer.writerow({"Дата": date.strftime("%d/%m/%Y"), "Доллар США": float(soup.find("CharCode", text = "USD").find_next_sibling("Value").get_text().replace(',','.')),
+            "Евро": float(soup.find("CharCode", text = "EUR").find_next_sibling("Value").get_text().replace(',','.')),
+            "Индийская рупия": round(float(soup.find("CharCode", text = "INR").find_next_sibling("Value").get_text().replace(',','.')) / 100, 5),
+            "Украинская гривна": round(float(soup.find("CharCode", text = "UAH").find_next_sibling("Value").get_text().replace(',','.')) / 10, 5)})
+
+def main():
+    #arr = input("Enter the array for task 1: ").split(" ")
+    #dictify(arr)
+    #first_set = input("Введите первое множество для Жаккара: ").split(" ")
+    #second_set = input("Введите второе множество для Жаккара: ").split(" ")
+    #jaccard(first_set, second_set)
+    Json_to_csv()
+    currency()
+
+main()
