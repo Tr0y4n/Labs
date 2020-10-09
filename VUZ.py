b91ae61c2a4723ca0bf9c1001d3409b381b95a6e
Tr0y4n <55826971+Tr0y4n@users.noreply.github.com>
2020-10-09 21:56:02 +0300

    5th task remake 4k 120 fps

--- VUZ.py
+++ VUZ.py
@@ -5,7 +5,7 @@ import datetime
 
 def dictify(arr):
     dic = { arr[i]:list(k for k in range(i, len(arr)) if arr[i] == arr[k]) for i in range(0,len(arr)) if arr.index(arr[i]) >= i}
-    print(dic)
+    return(dic)
 
 def jaccard(first_set, second_set):
     intersections = 0
@@ -13,7 +13,7 @@ def jaccard(first_set, second_set):
     for item in first_set:
         if item in second_set:
             intersections += 1
-    print(intersections / (len(first_set) + len(second_set) - intersections))
+    return(intersections / (len(first_set) + len(second_set) - intersections))
 
 def Json_to_csv():
     jname = "data.json"
@@ -32,31 +32,48 @@ def Json_to_csv():
                 for year in value:
                     writer.writerow({"item": dict['item'], "country": country, "year": year, "sales": value[year]})
 
-def currency():
+currencies = [
+    "Доллар США",
+    "Евро",
+    "Индийская рупия",
+    "Украинская гривна"
+]
 
+def currency():
     start = datetime.datetime.strptime("01/03/2020", "%d/%m/%Y")
     end = datetime.datetime.strptime("01/07/2020", "%d/%m/%Y")
     date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days)]
-    
-    with open("money.csv", "w", newline = "") as file:
-        columns = ["Дата", "Доллар США", "Евро", "Индийская рупия", "Украинская гривна"]
-        writer = csv.DictWriter(file, fieldnames = columns)
+
+
+    idResp = requests.get("http://www.cbr.ru/scripts/XML_val.asp?d=0")
+    soupParse = BeautifulSoup(idResp.content, 'xml')
+    currenciesId = {currency: soupParse.find("Name", text=currency).parent["ID"] for currency in currencies}
+
+
+    with open("money.csv",'w',newline="") as file:
+        columns = ["Дата"] + currencies
+        writer = csv.DictWriter(file, columns)
         writer.writeheader()
 
         for date in date_generated:
-            resp = requests.get("http://www.cbr.ru/scripts/XML_daily.asp?date_req=" + date.strftime("%d/%m/%Y"))
-            soup = BeautifulSoup(resp.content, "xml")
-            writer.writerow({"Дата": date.strftime("%d/%m/%Y"), "Доллар США": float(soup.find("CharCode", text = "USD").find_next_sibling("Value").get_text().replace(',','.')),
-            "Евро": float(soup.find("CharCode", text = "EUR").find_next_sibling("Value").get_text().replace(',','.')),
-            "Индийская рупия": round(float(soup.find("CharCode", text = "INR").find_next_sibling("Value").get_text().replace(',','.')) / 100, 5),
-            "Украинская гривна": round(float(soup.find("CharCode", text = "UAH").find_next_sibling("Value").get_text().replace(',','.')) / 10, 5)})
+            currencies_price_response = requests.get(f"http://www.cbr.ru/scripts/XML_daily.asp?date_req=" + date.strftime("%d/%m/%Y"))
+            mapRow = {columns[0]:date}
+            soupParse = BeautifulSoup(currencies_price_response.content, 'xml')
+
+            for currency, currencyId in currenciesId.items():
+                value = float(soupParse.find(ID=currencyId).find("Value").get_text().replace(',','.'))
+                nominal = int( soupParse.find(ID=currencyId).find("Nominal").get_text())
+                course = round(value / nominal, 5)
+                mapRow[currency] = course
+            writer.writerow(mapRow)
+
 
 def main():
     arr = input("Enter the array for task 1: ").split(" ")
-    dictify(arr)
+    print(dictify(arr))
     first_set = input("Введите первое множество для Жаккара: ").split(" ")
     second_set = input("Введите второе множество для Жаккара: ").split(" ")
-    jaccard(first_set, second_set)
+    print(jaccard(first_set, second_set))
     Json_to_csv()
     currency()
 
